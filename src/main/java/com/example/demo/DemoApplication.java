88ce0389f52e9fede964f384c7b428885456cd8a
Kiran-Pradha <kiranpradha30@gmail.com>
2025-06-17 09:16:59 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
 @RestController
 public class DemoApplication {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Java Spring Boot with Gradle!";
+        return "Hello from Dockerized Java App!";
     }
 
     @GetMapping("/health")
